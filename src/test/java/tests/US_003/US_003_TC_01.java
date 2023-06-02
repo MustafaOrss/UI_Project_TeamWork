954078041c8b16db56507b593d88cf03215eb3dc
Xerkerr <berkersaatci96@gmail.com>
2023-06-02 21:09:29 +0300

    us03guncellemesi

--- src/test/java/tests/US_003/US_003_TC_01.java
+++ src/test/java/tests/US_003/US_003_TC_01.java
@@ -1,4 +1,7 @@
 package tests.US_003;
 
 public class US_003_TC_01 {
+    //OUR SERVICES kısmındaki International Tour, Adventure Tour, Culture Tour,
+    // Business Tour, Health Tour, Regious Tour Fonksiyonlarının varlığının
+    // ve aktifliğinin kontrolü.
 }
---
+++ src/test/java/tests/US_003/US_003_TC_02.java
@@ -0,0 +1,6 @@
+package tests.US_003;
+
+public class US_003_TC_02 {
+    //DESTINATION kısmındaki Bangkok, Greenville, Argentina, Morocco, Italy, Turkey
+    // Fonksiyonlarının varlığının ve aktifliğinin kontrolü.
+}
