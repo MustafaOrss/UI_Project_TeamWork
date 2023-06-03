33aae061bf92c50ca6a070fdea00e733e25f5ae8
erdalakgun6 <erdalakgun6@gmail.com>
2023-06-03 16:07:25 +0300

    erdal29-01V1

--- src/test/java/tests/US_029/US_029_TC_01.java
+++ src/test/java/tests/US_029/US_029_TC_01.java
@@ -1,5 +1,12 @@
 package tests.US_029;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US_029_TC_01 {
 
     /*
@@ -7,4 +14,20 @@ public class US_029_TC_01 {
     2-https://qa.tripandway.com/admin/login adresine gidilir.
     3-Anasayfaya ulasildigi dogrulanir
      */
+    @Test
+    public void US029TC01(){
+        // chorme browser'ina framework de utilities icinde driver üzerinden gidilir.
+        Driver.getDriver().get(ConfigReader.getProperty("tripAndWayAdminUrl"));
+
+        // Anasayfaya ulasildigi dogrulanir
+        WebElement adminLoginElmenti = Driver.getDriver().findElement(By.xpath("//h1[text()='Admin Login']"));
+        String expectedIcerik = "Admin Login";
+        String actualYazi = adminLoginElmenti.getText();
+        Assert.assertTrue(actualYazi.contains(expectedIcerik));
+        System.out.println(actualYazi);
+
+        Driver.closeDriver();
+    }
+
+
 }
