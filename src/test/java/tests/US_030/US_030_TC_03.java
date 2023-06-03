9de1ace14ad8c7e13f70a705f6ab1cbbca520443
erdalakgun6 <erdalakgun6@gmail.com>
2023-06-03 14:45:36 +0300

    erdal v30.1

---
+++ src/test/java/tests/US_030/US_030_TC_03.java
@@ -0,0 +1,13 @@
+package tests.US_030;
+
+public class US_030_TC_03 {
+    /*
+    1-Browser'a gidilir.
+    2-https://qa.tripandway.com/admin/login adresine gidilir.
+    3-Mail ve Sifre girilir.
+    4-Login click yapılır.
+    5-Admin Panelin de bulunan Destinations butonuna tiklanir.
+    6-Sayfanin sol ust kosesinde "Destinations"
+    yazisinin oldugu dogrulanir.
+     */
+}
---
+++ src/test/java/tests/US_030/US_030_TC_05.java
@@ -0,0 +1,15 @@
+package tests.US_030;
+
+public class US_030_TC_05 {
+    /*
+    1-Browser'a gidilir.
+    2-https://qa.tripandway.com/admin/login adresine gidilir.
+    3-Mail ve Sifre girilir.
+    4-Login click yapılır.
+    5-Destinations butonuna basilir
+    6-Destination sayfasinda eklenen Destination satirina gelinir.
+    7-Düzenlenle butonuna tiklanir
+    8-Eklenen destination yazisinin update butonuna basilir
+    9-'Destination is updated successfully!' uyarisi görülür.
+     */
+}
