110810626877f520e224e188744fa8754b1911e6
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-06-05 19:14:23 +0300

    Demet/05.06/UserDashboardPageEklendi

--- src/test/java/tests/US_021/US_021_TC_01.java
+++ src/test/java/tests/US_021/US_021_TC_01.java
@@ -1,4 +1,30 @@
 package tests.US_021;
 
-public class US_021_TC_01 {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.UserDashboard;
+import pages.UserHomepage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.UserTestBaseRapor;
+
+public class US_021_TC_01 extends UserTestBaseRapor {
+   UserHomepage userHomepage = new UserHomepage();
+   @Test
+   public void TC_01(){
+      extentTest = extentReports.createTest("Tripandway anasayfa testi",
+              "Kullanici sayfaya ulasabilmeli");
+
+      // 1- Launch Browser
+      // 2- Go to https://qa.tripandway.com/
+      // TestBaseRapor'dan URL giris yapildi
+
+
+      // 3- Anasayfaya erisildigi dogrulanmalidir
+      String actualTitle= Driver.getDriver().getTitle();
+      String expectedHomePageTitle = ConfigReader.getProperty("expectedHomePageTitle");
+      Assert.assertTrue(actualTitle.contains(expectedHomePageTitle));
+      extentTest.pass("Belirlenen " + expectedHomePageTitle + " aranacak title arama sonucunda expected icerik oldugu test edildi");
+
+   }
 }
