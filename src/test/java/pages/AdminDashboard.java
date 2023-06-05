1537505aca3b297099f0bf656ba187bd321af4cc
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-06-05 13:09:17 +0300

    merve

--- src/test/java/pages/AdminDashboard.java
+++ src/test/java/pages/AdminDashboard.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,4 +10,27 @@ public class AdminDashboard {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    // Admin Anasayfa >> Email Address
+    @FindBy(id = "email")
+    public WebElement emailAddressButonu;
+
+    // Admin Anasayfa >> Password
+    @FindBy(xpath = "//input[@type='password']")
+    public WebElement passwordButonu;
+
+    // Admin Anasayfa >> Login
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement loginButonu;
+
+    // Dashboard
+    @FindBy(xpath = "//h1[text()='Dashboard']")
+    public WebElement dashboardElementi;
+
+    // General Settings
+    @FindBy(xpath = "//span[text()='General Settings']")
+    public WebElement generalSettingsElementi;
+
+    // Page Settings
+    @FindBy(xpath = "//span[text()='Page Settings']")
+    public WebElement pageSettingsElementi;
 }
