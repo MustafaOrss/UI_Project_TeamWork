a30663c682403e7ddc65277031e795bad805a23a
Mustafa Ors <mustafaors0101@icloud.com>
2023-06-06 11:26:36 +0300

    mustafa/06.06/US_34_TC01TC02TC03TC04ClasslariEklendi

--- src/test/java/tests/US_034/US_034_TC_01.java
+++ src/test/java/tests/US_034/US_034_TC_01.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import pages.AdminDashboard;
 import utilities.AdminTestBaseRapor;
+import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
@@ -16,33 +17,33 @@ import java.util.Properties;
 
 public class US_034_TC_01 extends AdminTestBaseRapor {
 
+
+    AdminDashboard adminDashboard = new AdminDashboard();
     @Test
     public void TC_01() throws InterruptedException, IOException {
         extentTest=extentReports.createTest("Tripandway admin sayfasina gidis ve email giris testi",
-                 "Kullanici ana sayfaya gidip email girebilmeli");
+                 "Admin ana sayfaya gidip email ve password ile login olabilmeli.");
 
+        //1-Launch Browser
+        //2-"https://qa.tripandway/com/admin/login" adresine gidebilmeliyim.
 
-        // "https://qa.tripandway/com/admin/login" adresine gidebilmeliyim." (adminTestBaseRapor class'ini extend ederek gidildi.)
-        AdminDashboard adminDashboard = new AdminDashboard();
-        // Admin Email ve Password datalarini dinamik olarak kullanabilmek icin Properties dosyasindan obje olusturabilmeliyim.
-        Properties properties = new Properties();
-        // Properties kutuphanesini yuklemek icin FileInputStream objesi olusturabilmeliyim.
-        FileInputStream fileInputStream = new FileInputStream("configuration.properties");
-        // Properties ve FileInputStream dosyalarini iceri aktarabilmeliyim.
-        properties.load(fileInputStream);
-        // Class'da kullanmak icin Properties objesinden Email datasini bir degiskene atayabilmeliyim
-        String adminEmail = properties.getProperty("admin28Email");
-        // Class'da kullanmak icin Properties objesinden Password datasini bir degiskene atayabilmeliyim
-        String adminPassword = properties.getProperty("adminPassword");
+        extentTest.info("Admin " + ConfigReader.getProperty("tripAndWayAdminUrl")+ "Ana sayfasina gider");
 
-        // Email kismina gerekli datalari yazabilmeliyim.
-        adminDashboard.adminEmailKutusu.sendKeys(adminEmail);
-        // Password kismina gerekli datalari yazabilmeliyim.
-        adminDashboard.adminPasswordKutusu.sendKeys(adminPassword);
-        // Admin Email'i ve Password'u ilgili kisimlara girildikten sonra login butonuna click yapabilmeliyim.
+        //3-Email kismina gerekli datalari yazabilmeliyim.
+        adminDashboard.adminEmailKutusu.sendKeys(ConfigReader.getProperty("admin28Email"));
+        extentTest.info("Email adres girildi.");
+
+        //4-Password kismina gerekli datalari yazabilmeliyim.
+        adminDashboard.adminPasswordKutusu.sendKeys(ConfigReader.getProperty("adminPassword"));
+        extentTest.info("Password girildi");
+
+        //5-Admin Email'i ve Password'u ilgili kisimlara girildikten sonra login butonuna click yapabilmeliyim.
         adminDashboard.adminLoginButonu.click();
-        // Admin sayfasina ulastirildigimi dogruluyabilmeliyim.
+        extentTest.info("Login butonu tiklandi");
+
+        //6-Admin sayfasina ulastirildigimi dogruluyabilmeliyim.
         adminDashboard.dashboardLocate.isDisplayed();
+        extentTest.info("Ana sayfaya ulasildigi dogrulandi");
         ReusableMethods.wait(1);
 
 
--- src/test/java/tests/US_034/US_034_TC_03.java
+++ src/test/java/tests/US_034/US_034_TC_03.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AdminDashboard;
 import utilities.AdminTestBaseRapor;
+import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
@@ -13,64 +14,72 @@ import java.util.Properties;
 import java.util.Set;
 
 public class US_034_TC_03 extends AdminTestBaseRapor {
+
+    AdminDashboard adminDashboard = new AdminDashboard();
     @Test
     public void TC_03() throws InterruptedException, IOException {
 
+        extentTest = extentReports.createTest("Tripandway admin sayfasina gidis, " +
+                        "email giris ve order ve invoice bolumune erisim",
+                "Kullanici ana sayfaya gidip email ve password ile login olabilmeli, " +
+                        "order ve invoice bolumune erisebilmeli");
 
-        extentTest = extentReports.createTest("Tripandway admin sayfasina gidis, email giris ve order bolumune erisim",
-                "Kullanici ana sayfaya gidip email girebilmeli ve order bolumune erisebilmeli");
+        //1-Launch Browser
+        //2-"https://qa.tripandway/com/admin/login" adresine gidebilmeliyim.
 
+        extentTest.info("Admin " + ConfigReader.getProperty("tripAndWayAdminUrl")+ "Ana sayfasina gider");
 
-        // "https://qa.tripandway/com/admin/login" adresine gidebilmeliyim." (adminTestBaseRapor class'ini extend ederek gidildi)
-        AdminDashboard adminDashboard = new AdminDashboard();
-        // Admin Email ve Password datalarini dinamik olarak kullanabilmek icin Properties dosyasindan obje olusturabilmeliyim.
-        Properties properties = new Properties();
-        // Properties kutuphanesini yuklemek icin FileInputStream objesi olusturabilmeliyim.
-        FileInputStream fileInputStream = new FileInputStream("configuration.properties");
-        // Properties ve FileInputStream dosyalarini iceri aktarabilmeliyim.
-        properties.load(fileInputStream);
-        // Class'da kullanmak icin Properties objesinden Email datasini bir degiskene atayabilmeliyim
-        String adminEmail = properties.getProperty("admin28Email");
-        // Class'da kullanmak icin Properties objesinden Password datasini bir degiskene atayabilmeliyim
-        String adminPassword = properties.getProperty("adminPassword");
+        //3-Email kismina gerekli datalari yazabilmeliyim.
+        adminDashboard.adminEmailKutusu.sendKeys(ConfigReader.getProperty("admin28Email"));
+        extentTest.info("Email adres girildi.");
 
-        // Email kismina gerekli datalari yazildi.
-        adminDashboard.adminEmailKutusu.sendKeys(adminEmail);
-        // Password kismina gerekli datalari yazildi.
-        adminDashboard.adminPasswordKutusu.sendKeys(adminPassword);
-        // Admin Email'i ve Password'u ilgili kisimlara girildikten sonra login butonuna click yapildi.
+        //4-Password kismina gerekli datalari yazabilmeliyim.
+        adminDashboard.adminPasswordKutusu.sendKeys(ConfigReader.getProperty("adminPassword"));
+        extentTest.info("Password girildi");
+
+        //5-Admin Email'i ve Password'u ilgili kisimlara girildikten sonra login butonuna click yapabilmeliyim.
         adminDashboard.adminLoginButonu.click();
-        // Admin sayfasina ulastirildigimi dogruluyabilmeliyim.
+        extentTest.info("Login butonu tiklandi");
+
+        //6-Admin sayfasina ulastirildigimi dogruluyabilmeliyim.
         adminDashboard.dashboardLocate.isDisplayed();
-        // Order Butonuna tiklayabilmeyim
+        extentTest.info("Ana sayfaya ulasildigi dogrulandi");
+        //7-Order Butonuna tiklayabilmeliyim
         adminDashboard.orderButonuLocate.click();
-        // Order bolumunde oldugumu dogrulayabilmeliyim.
+        extentTest.info("Order butonu tiklandi");
+
+        //8-Order bolumune gecildigini dogrulayabilmeliyim.
         adminDashboard.orderBodyLocate.isDisplayed();
-        // Invoice bolumune tiklandiginda yeni bir window'a yonlendirdigi icin
+        extentTest.info("Order bolumune gecildigi dogrulandi");
+        //Invoice bolumune tiklandiginda yeni bir window'a yonlendirdigi icin
         String ilkWindowHandle = Driver.getDriver().getWindowHandle();
         // uzerinde olunan pencerenin window handle degeri alindi
+        // uzerinde olunan pencerenin window handle degeri alindi
         System.out.println(ilkWindowHandle);
-        // Invoice butonuna tiklayabilmeliyim
+        //9- Invoice butonuna tiklayabilmeliyim
         adminDashboard.invoiceButonuLocate.click();
+        extentTest.info("Invoice butonu tiklandi");
 
         String ikinciWindowWHD = "";
         // windowlarin degerlerini alir(icinde 2 tane WHD var)
         Set<String> windowDegerlerSeti = Driver.getDriver().getWindowHandles();
+        extentTest.info("Butun pencerelerin window handle degerleri alindi");
 
         for (String eachWHD : windowDegerlerSeti
         ) {
-            // ilk window'a esit olmayani ikinci window'a atayalim
+            // ilk window'a esit olmayan ikinci window'a atanir
             if (!eachWHD.equals(ilkWindowHandle))
                 ikinciWindowWHD = eachWHD;
+            extentTest.info("ilk window'a esit olmayan window handle degeri ikinci window'a atandi");
         }
 
-        // ikinci window'daki "Invoice" bolumune gecelim
+        //10-ikinci window'daki "Invoice" bolumune gecebilmeliyim
         Driver.getDriver().switchTo().window(ikinciWindowWHD);
-        // 3 saniye bekleyelim
+        extentTest.info("Invoice bolumune gecildi");
         ReusableMethods.wait(3);
-        // ikinci window'daki "Invoice" bolumunde olundugu dogrulandi
+        //11-ikinci window'daki "Invoice" bolumune gecildigini dogrulayabilmeliyim.
         Assert.assertTrue(adminDashboard.orderInvoiceLocate.isDisplayed());
-
+        extentTest.info("Invoice bolumune gecildigi dogrulandi");
 
     }
 
