cfb705c5d66b25ee94f60a316c811987fafa3527
Demet Gundogdu <demetgundogdu4@gmail.com>
2023-06-06 15:03:26 +0300

    demet/06/06/userdashbordEklemeYapıldı/US24-TC02/US22-TC02Eklendi

---
+++ src/test/java/tests/US_022/US_022_TC_02.java
@@ -0,0 +1,70 @@
+package tests.US_022;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.UserDashboard;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.UserTestBaseRapor;
+
+import java.time.Duration;
+
+public class US_022_TC_02 extends UserTestBaseRapor {
+    @Test
+    public void TC_02() throws InterruptedException {
+        UserDashboard userDashboard=new UserDashboard();
+     extentTest=extentReports.createTest("Uptade Password testi","Kullanıcı login iken şife degiştirebilmeli");
+        //1-launch browser
+        //2-Go to https://qa.tripandway.com/traveller/login
+        extentTest.info("kullanıcı"+ ConfigReader.getProperty("tripAndWayUrl")+"anasayfaya gider");
+        userDashboard.cookiesButonu.click();
+        userDashboard.ilkLoginButonu.click();
+        //3-Email kısmına ilgili data yazılır
+         userDashboard.emailAddressButonu.sendKeys(ConfigReader.getProperty("userEmail"));
+         extentTest.info("kullanıcı mail adresini girdi");
+        //4-Password kısmına ilgili data yazılır
+        userDashboard.passwordButonu.sendKeys(ConfigReader.getProperty("userPassword"));
+        extentTest.info("kullanıcı sıfre gırdı");
+        //5-Login butonuna tıklanır
+        userDashboard.loginButonu.click();
+        extentTest.info("login butonuna tıklanır");
+        //6-"Uptade Password" butonunun gorunurlugu
+        // dogrulanmalı
+        Assert.assertTrue(userDashboard.updatePasswordButonu.isDisplayed());
+        extentTest.info("Uptade Password butonu göründü");
+        //7-"Uptade Password " butonuna tıkla
+        userDashboard.updatePasswordButonu.click();
+        extentTest.info("Uptade Password butonu tıklandı");
+        //8-new password 'un textbox 'ının gorunurlugunu dogrulanmalı
+        Thread.sleep(2000);
+         Assert.assertTrue(userDashboard.newPasswordKutusu.isDisplayed());
+         extentTest.info("new password textbox kutusu göründü");
+        //9-new password 'un textbox 'ına yenı sıfre gır
+        userDashboard.newPasswordKutusu.sendKeys(ConfigReader.getProperty("newPassword"));
+        extentTest.info("new password textbox kutusuna yenı dıfre yazıldı");
+        //10-"Uptade" butonunun gorunurlugunu dogrula
+        Assert.assertTrue(userDashboard.updateButonu.isDisplayed());
+        extentTest.info("Uptade butonu gorundu");
+
+        //11-"Uptade"butonuna tıkla
+        userDashboard.updateButonu.click();
+        extentTest.info("Uptade butonu tıklandı");
+
+        //12-Password is updated successfully yazısının
+        //gorunurlugu dogrulanmalı//
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
