f4045fbe7cd8597c268c973b8af3e0c9d911f69b
Merve Nur Taşan <127309624+mervenurtasann@users.noreply.github.com>
2023-06-08 00:40:41 +0300

    Delete US_002_TC_02.java

--- src/test/java/tests/US_002/US_002_TC_02.java
+++
@@ -1,6 +0,0 @@
-package tests.US_002;
-
-public class US_002_TC_01 {
-    //Header in sağ üst köşesindeki Registration ve login butonlarının varlığını kontrol edip
-    // eğer varsa aktif olduğunu kontrol etmeliyim.
-}
