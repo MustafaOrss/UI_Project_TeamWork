2ec3363451f8a9dc01673963d0ded82efba9288c
Mustafa Ors <mustafaors0101@icloud.com>
2023-06-09 16:26:19 +0300

    zehra/09.06/2023/UserHomePageTC01denTC07dahilArasiClasslar

--- src/test/java/tests/US_012/US_012_TC_01.java
+++ src/test/java/tests/US_012/US_012_TC_01.java
@@ -15,17 +15,18 @@ public class US_012_TC_01 extends UserTestBaseRapor {
         extentTest=extentReports.createTest("hizmetler sayfasının görünürlüğü testi",
                 "hizmetler sayfasının görünürlüğü test edildi");
        // 1-Açılan browser https://qa.tripandway.com/adresini yazıp anasayfaya erişilebilmelidir.
+        extentTest.info("Kullanici " + ConfigReader.getProperty("tripAndWayUrl")+ "Ana sayfasina gider");
         String actualTitle = Driver.getDriver().getTitle();
         String expectedHomePageTitle = ConfigReader.getProperty("expectedHomePageTitle");
         Assert.assertTrue(actualTitle.contains(expectedHomePageTitle));
         extentTest.info("hizmetler sayfasına gidildi");
-        //2-"SERVİCES" elementine tıklanır.
-        homepage.servicespage.click();
-        extentTest.pass("SERVİVES elementine tıklandı");
+        //2-"Services" elementine tıklanır.
+        homepage.servicesButon.click();
+        extentTest.pass("Services elementine tıklandı");
 
-        // 3-"SERVİCESé sayfasının açıldığı test edilir
+        // 3-Services sayfasının açıldığı test edilir
         String actualservicestitle = Driver.getDriver().getTitle();
-        String expectedservicesTitle = ConfigReader.getProperty("expectedservicesTitle");
+        String expectedservicesTitle = "Services";
         Assert.assertTrue(actualservicestitle.contains(expectedservicesTitle));
         extentTest.pass("services sayfası doğrulandı");
 
