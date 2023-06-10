364c1bc87c0d3e1f2f3b29d93e93f2b76ab95ce9
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-06-10 19:18:14 +0300

    merve/10.06/added

--- src/test/java/tests/US_009/US_009_TC_02.java
+++ src/test/java/tests/US_009/US_009_TC_02.java
@@ -6,7 +6,7 @@ import utilities.*;
 public class US_009_TC_02 extends UserTestBaseRapor {
     UserHomepage userHomepage=new UserHomepage();
     @Test
-    public void tc_01(){
+    public void tc_02(){
         // 1-Açılan browser https://qa.tripandway.com/adresini yazıp anasayfaya erişilebilmelidir.
         extentTest = extentReports.createTest("\"Newsletter\" Grubuna Katilma Testi",
                 "Ziyaretci \"Newsletter\" Grubuna Katilabilmelidir");
@@ -16,7 +16,7 @@ public class US_009_TC_02 extends UserTestBaseRapor {
         ReusableMethods.wait(2);
         extentTest.info("Footer bölümüne gidildi");
         //3-mail haber grubu(newsletter) kısmındaki Email kutusuna geçerli bir email adresi yazılır.
-        userHomepage.newsletterEmailKutusu.sendKeys("nazli1@gmail.net");
+        userHomepage.newsletterEmailKutusu.sendKeys("dfga8567@gmail.com");
         extentTest.info("Gecerli bir email adresi girildi");
         //4- "Submit" butonuna tıklanır
         userHomepage.newsletterEmailKutusu.submit();
@@ -25,5 +25,28 @@ public class US_009_TC_02 extends UserTestBaseRapor {
         Assert.assertTrue(userHomepage.toActivatePencereElementi.isDisplayed());
         extentTest.pass("\"To activite your subscription please check your email and follow instruction there.\" " +
                 "yazisinin gorundugu doğrulandı");
+        Driver.getDriver().get("https://accounts.google.com/AccountChooser/signinchooser?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser");
+        extentTest.info("Gmail sayfasina gidildi");
+        userHomepage.gmailEmail.sendKeys("dfga8567@gmail.com");
+        extentTest.info("Gmail sayfasinda email yazildi");
+        userHomepage.ileri.click();
+        ReusableMethods.wait(4);
+        userHomepage.gmailSifre.sendKeys("Yes_12345");
+        extentTest.info("Gmail sayfasinda sifre yazildi");
+        userHomepage.ileri.click();
+        Assert.assertTrue(userHomepage.gmailLogosu.isDisplayed());
+        extentTest.pass("Gmail sayfasina ulasildi");
+        userHomepage.gmailSearchKutusu.sendKeys("Tripandway");
+        userHomepage.gmailSearchKutusu.submit();
+        extentTest.info("Arama kutusuna Tripandway yazildi ve enter'a basildi");
+        ReusableMethods.wait(1);
+        String actualMail = userHomepage.tripandwayMail.getText();
+        String expectedMail = "Tripandway";
+
+        Assert.assertEquals(expectedMail,actualMail);
+        extentTest.info("Tripandway maili dogrulandi");
+
+
+
     }
 }
